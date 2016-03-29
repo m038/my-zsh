234056f125b29cdbc808a09778019486e69509b5
Mischa Gorinskat <m.gorinskat@gmail.com>
2016-03-29 14:41:54 +0200

    Updates docker php7 command

--- docker.zsh
+++ docker.zsh
@@ -11,7 +11,7 @@ alias dha='dh `dpsq`'
 
 alias dc='docker-compose'
 
-alias dphp7='docker run -it --rm --name php7 -v "$(pwd)":/tmp -w /tmp php:7.0.2-cli'
+alias dphp7='docker run -it --rm --name php7 -v "$(pwd)":/tmp -w /tmp php:7.0.3-cli'
 
 compdef -e 'words[1]=(docker exec); service=docker; (( CURRENT+=2 )); _docker' dbash
 dbash () { docker exec -t -i $1 bash }
